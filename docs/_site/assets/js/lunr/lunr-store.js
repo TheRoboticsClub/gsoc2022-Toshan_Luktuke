7a8e722d78924e4c1bb7491963378ea48c751792
toshan-luktuke <toshan1603@gmail.com>
2022-09-02 19:51:01 +0530

    Updates to _site elements

--- docs/_site/assets/js/lunr/lunr-store.js
+++ docs/_site/assets/js/lunr/lunr-store.js
@@ -1,7 +1,85 @@
 var store = [{
-        "title": "Welcome to Jekyll!",
-        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "title": "Installing JDE Drones",
+        "excerpt":"Getting started with my GSoC period, one of my goals during the project was to make drone applications using Visual Circuit. JdeRobot’s Academy already has some great exercises related to drones and their basic concepts, my job was to get a solution running using Visual Circuit. The challenge here was...","categories": ["gsoc"],
         "tags": [],
-        "url": "/gsoc2022-Toshan_Luktuke/jekyll/update/2022/05/26/welcome-to-jekyll.html",
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/installing-jde-drone/",
+        "teaser": null
+      },{
+        "title": "Getting Code Running on Drones",
+        "excerpt":"After I got the drones running now it was time to actually write the code to solve some exercises. The goal for this week of the GSoC period was to get 1-2 applications of the drones solved using Visual Circuit. But before I could do that, I had to get...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/getting-code-running-on-drones/",
+        "teaser": null
+      },{
+        "title": "Week 2: Community Bonding",
+        "excerpt":"In my previous posts I had highlighted the challenges I faced while installing Jde Drones and getting code running on it. Next after these steps was to solve some of the JdeAcademy exercises. The main goal for this week was to get the solutions running using Visual Circuit. Work Approach...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/community-bonding-week-2/",
+        "teaser": null
+      },{
+        "title": "Week 3: Community Bonding",
+        "excerpt":"This was the last week of community bonding, however due to other commitments in college I had a hard time working on Visual Circuit. The major work done this week consisted mainly of improving the solution to the Labyrinth Escape exercise. I solved it using Visual Circuit as well, however...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/community-bonding-week-3/",
+        "teaser": null
+      },{
+        "title": "Week 1: Coding Period",
+        "excerpt":"This week marked the official beginning of the coding period. Going by last week’s set goals I started on the video creation part first and foremost. In order to portray a Visual Circuit solution that was good to view as an example, I first had to solve a persistent bug...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-1/",
+        "teaser": null
+      },{
+        "title": "Week 2: Coding Period",
+        "excerpt":"This week was about identifying and solving some bugs I had noticed during usage of the tool. I also started with the work template updation. Collectstatic Going by the goals set last week and the advice of Suhas the first thing I did was to research the collectstatic step in...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-2/",
+        "teaser": null
+      },{
+        "title": "Week 3: Coding Period",
+        "excerpt":"This week I finally finished the remaining templates from last week. I also managed to upload my created videos to the JdeRobot YouTube Channel. Template Updation Continuing from where I left off last week, this week I finished all the remaining template blocks. A list of them would be Teleoperator,...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-3/",
+        "teaser": null
+      },{
+        "title": "Week 4: Coding Period",
+        "excerpt":"This week marked a focus on completing the remaining PRs from last week. I also finished adding a new IMU block and worked on general share() and read() functions. IMU Block An IMU (Inertial Measurement Unit) is a sensor commonly found in drones that detects the orientation and the angular...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-4/",
+        "teaser": null
+      },{
+        "title": "Week 5: Coding Period",
+        "excerpt":"This was a week where I started to work on the Finite State Machine that I had mentioned in my proposal. ## FSM The start was simple enough, first I had to find a world to run the robot in. My mentors suggessted the Vacuum Cleaner exercise from Robotics Academy...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-5/",
+        "teaser": null
+      },{
+        "title": "Week 6: Coding Period",
+        "excerpt":"This week was quite hectic for me. My college started offline after a while, due to travelling and a few other events the time I could spend on Visual Circuit was reduced to a certain exent. Still, I managed to make some progress on last week’s goals. FSM I continued...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-6/",
+        "teaser": null
+      },{
+        "title": "Week 7: Coding Period",
+        "excerpt":"Another pretty busy week for me personally. This led to a bit of slowdown in the work done. However I started with the documentation editing through Jinja and tried using Locks for the FSM. Documentation I started looking into extending and editing pdoc for our purposes. I found out that...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-7/",
+        "teaser": null
+      },{
+        "title": "Week 8: Coding Period",
+        "excerpt":"I made more progress with the documentation and some other auxillary bug fixes this week. Documentation Continuing on last week’s images, this week I wrote the basic documentation for all of the blocks in Visual Circuit. It was done by adding a docstring to the main() function of the Python...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-8/",
+        "teaser": null
+      },{
+        "title": "Week 9: Coding Period",
+        "excerpt":"This week saw the completion of the FSM video and the solution to the block size bug. Block Size The problem lay in the fact that you don’t have a resize event listener in HTML or React. Due to this Suhas made resize events fire using the onMouseDown listener in...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-9/",
+        "teaser": null
+      },{
+        "title": "Week 10: Coding Period",
+        "excerpt":"This week I looked into MultiProcessing Conditions and finished the video of the FSM. Video After consulting with Jose Maria Canas, I added extra slides and description to the video. Finally I capped it off by adding subtitles and background music. As my video editing skills have improved somewhat, this...","categories": ["gsoc"],
+        "tags": [],
+        "url": "/gsoc2022-Toshan_Luktuke/gsoc/coding-period-week-10/",
         "teaser": null
       }]
